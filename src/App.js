ad422c19726ec5930c398df2bac77af039221930
IFTE-13 <mariamnoor1290@gamil.com>
2022-09-07 15:32:04 +0600

    speechSynthesis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, {useState, useMemo, useEffect} from 'react';
+import Result from "./Result";
+
+const synth = window.speechSynthesis;
+
 
 const App = () => {
+  const voices = useMemo(() => synth.getVoices(), [])
+  const [voiceSelected, setVoiceSelected] = useState("Google US English");
+  const [text, setText] = useState("");
+  const [isSpeaking, setIsSpeaking] = useState("");
+
+  const startSpeech = (text) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    const voice = voices.find(voice => voice.name === voiceSelected);
+    utterance.voice = voice;
+    synth.speak(utterance);
+  }
+
+  const handleSpeech = () => {
+    if(!text.trim()) return;
+    if(!synth.speaking){
+      startSpeech(text);
+      setIsSpeaking("speak");
+    }else{
+      synth.cancel();
+    }
+
+    setInterval(() => {
+      if(!synth.speaking){
+        setIsSpeaking("");
+      }
+    }, 100);
+  }
+  
   return (
-    <div>App</div>
+    <div className='container'>
+      <h1>English Dictionary</h1>
+
+      <form>
+        <div className='row'>
+          <textarea cols="30" rows="4" placeholder="Enter text/" value={text} onChange={e => setText(e.target.value)}></textarea>
+          <div className='voices-icon'>
+            <div className='select-voices'>
+              <select value={voiceSelected} onChange={
+                e=> setVoiceSelected(e.target.value)
+              }>
+                {
+                  voices.map(voice => (<option key={voice.name}
+                    value={voice.name}>{voice.name}</option>))
+                }
+              </select>
+            </div>
+            <i onClick={handleSpeech} className={`fa-solid fa-volume-high  ${isSpeaking}`}/>
+          </div>
+        </div>
+      </form>
+
+
+<Result/>
+    </div>
   )
 }
 
---
+++ src/Result.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const result = () => {
+  return (
+    <ul>
+        <li className='world'>
+            <h2>Hello</h2>
+            <span>/ha'lau/ /ha'lau/</span>
+        </li>
+        <li className='contain'>
+            <h3>noun</h3>
+
+            <div className='details meaning'>
+                <h3>Meaning</h3>
+                <p>hello or an equivalet greeting.</p>
+            </div>
+            <div className='details synonyms'>
+                <h3>synonyms</h3>
+                <span>greetings, greestins, greetings</span>
+            </div>
+        </li>
+    </ul>
+  )
+}
+
+export default result
